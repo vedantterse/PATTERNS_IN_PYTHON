6b75232329bf45d61c97487939c617cdccb843f7
vedantterse <vedantterse75575@gmail.com>
2024-03-05 00:40:49 +0530

    star patterns updated

--- characterpattern.py
+++ characterpattern.py
@@ -1,15 +1,13 @@
 n=4
 q = 64
+p = q + n
 for i in range(n):
-    p = q + n
+
     for j in range(i + 1):
         print(chr(p), end=' ')
-        p -= 1
+    p -= 1
     print()
 # D
 # D C
 # D C B
 # D C B A
-
-
-
